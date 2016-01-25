4425784b297b4d35b3e9c9a7c5d5d126b0df7e14
Martin Cross <mncross@gmail.com>
2016-01-25 13:14:45 -0600

    completed query 1

---
+++ 1.sql
@@ -0,0 +1,9 @@
+-- Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+
+SELECT
+	FirstName||' '||LastName as CustomerFullName,
+	CustomerId,
+	Country
+FROM Customer
+WHERE
+	Country != 'USA'
